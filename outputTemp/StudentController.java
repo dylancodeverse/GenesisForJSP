137339209870b3418295ca7ef521e487a49aec09
MisaFenosoa <ranarijaonamisafenosoa@gmail.com>
2024-03-20 10:55:37 +0300

    ok variable spec controller

--- outputTemp/StudentController.java
+++ outputTemp/StudentController.java
@@ -25,3 +25,82 @@ public class StudentController {
 
     private PromotionModel promotionModel ;
 
+    public StudentController(StudentService studentService
+
+            ,PromotionService promotionService
+
+        ) {
+
+        this.studentService = studentService;        
+
+        this.PromotionService = promotion;
+
+    }        
+
+    @GetMapping("/list")
+    public String listStudents(Model model) {
+
+        model.addAttribute("students", studentService.findAll());
+
+        return "pages/student/list";
+    }        
+
+    @GetMapping("/add")
+    public String showAddStudentForms(Model model) {
+
+        model.addAttribute("promotions", promotionService.findAll());
+
+        return "pages/student/list";
+    }        
+
+    @PostMapping("/add")
+    public String addStudent(
+
+            @RequestParam String promotionId
+
+            , Student student){
+
+        Promotion promotion = new Promotion();
+        promotion.setId(Integer.parseInt(promotionId));
+        student.setPromotion(promotion);
+
+        studentService.save(student);
+        return "redirect:/students/list";
+    }    
+
+    @GetMapping("/edit{id}")
+    public String showEditStudentForm(@PathVariable id id, Model model) {
+        Student student = studentService.findById(id);
+
+        List<Promotion> promotions = promotionService.findAll();
+        promotions.remove(student.getPromotion());     
+        model.addAttribute("promotions", promotions);
+
+        model.addAttribute("student", student);
+        return "pages/student/edit";
+    }        
+
+    @PostMapping("/edit")
+    public String editStudent(
+
+            @RequestParam String promotionId
+
+            , Student student){
+
+        if (student.getId() != null          
+
+               && promotionId != null
+
+            ){
+
+            Promotion promotion = new Promotion();
+            promotion.setId(promotionId);    
+            student.setPromotion(promotion);    
+
+            studentService.save(student);
+        }
+        return "redirect:/students/list";
+    }        
+
+}
+
